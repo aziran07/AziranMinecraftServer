999ac0c8adf5f039ef166637fe3a573cf99caf03
aziran07 <pilon1945@naver.com>
2024-07-08 04:58:55 +0000

    Fix backup.sh

--- mc_backup.sh
+++ mc_backup.sh
@@ -4,4 +4,4 @@
 timevar=$(date +"%Y-%m-%d_%T")
 cd /home/pilon1945/AziranMinecraftServer
 tar -cf ./minecraft_backups/${timevar}_world.tar ./server-data/world
-find ./minecraft_backups/ -type f -mmin +290 | xrgs rm -f
+find ./minecraft_backups/ -type f -mmin +290 | xargs rm -f
